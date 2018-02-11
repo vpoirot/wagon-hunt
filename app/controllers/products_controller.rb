b01809ab2d37f314b3bcae68aba7e5952b5e9f30
Vincent Poirot <vincent.poirot@gmail.com>
2018-02-11 23:11:03 +0100

    add CRUD & ressources

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,20 @@ class ProductsController < ApplicationController
   def show
     @product =Product.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+
   get "/team" => "pages#team"
   get "/contact" => "pages#join_us"
-  get "/products" => "products#index"
-  get "/products/:id" => "products#show"
+
+  resources :products
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
