1bcd9382c1157eabb835f0cf4a28e93ab012166f
Vincent Poirot <vincent.poirot@gmail.com>
2018-02-12 13:03:15 +0100

    refacto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show,:edit,:update,:destroy]
+
   def index
     @products= Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
@@ -21,11 +22,9 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to products_path
     else render :edit
@@ -33,7 +32,6 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
     redirect_to products_path
   end
@@ -41,6 +39,11 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:name,:url)
+  end
+
+  private
+  def find_product
+    @product = Product.find(params[:id])
   end
 
 end
