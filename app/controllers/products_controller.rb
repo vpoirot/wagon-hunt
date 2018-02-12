32b00f7bbb61720b4bc3f43fef0f31c98ff3bc07
Vincent Poirot <vincent.poirot@gmail.com>
2018-02-12 16:01:54 +0100

    frontend done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,7 +38,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name,:url)
+    params.require(:product).permit(:name,:url,:tagline)
   end
 
   private
